1c2a5cd98ae5374aff40033f2ed759a23ad7e65c
Emma Grasmeder <e.grasmeder@gmail.com>
2015-06-21 14:20:55 -0400

    Add tests and basic code for acceleration.

--- hoo/cars.py
+++ hoo/cars.py
@@ -3,5 +3,8 @@ class Car:
         self.x_loc = 0
         self.y_loc = 0
         self.speed = 0
-        self.direction = 0
+        self.direction = None
+    def accelerate(self, acc_percent):
+        if self.direction is None:
+            raise(AttributeError) #, "Acceleration function requires `direction`")
 
--- tests/unit/test_cars.py
+++ tests/unit/test_cars.py
@@ -1,4 +1,5 @@
 from unittest import TestCase, main
+import random
 from hoo import cars
 
 
@@ -13,7 +14,20 @@ class TestCarBasicFunctionality(TestCase):
         self.assertIsNotNone(car.speed)
         self.assertIsNotNone(car.direction)
 
-#    def test_car_
+class Test_AccelerateChangesCarsLocation(TestCase):
+    def test_cars_without_directions_cant_accelerate(self):
+        car = cars.Car()
+        car.direction=None
+        self.assertRaises(AttributeError, car.accelerate(.5))
+
+    def test_that_the_car_can_drive_north(self):
+        car = cars.Car()
+        car.direction=0
+        old_location = tuple((car.x_loc, car.y_loc))
+        car.accelerate(random.choice([x*.01 for x in range(1,99)]))
+        new_location = tuple((car.x_loc, car.y_loc))
+        self.assertNotEqual(old_location, new_location)
+
 
 if __name__ == "__main__":
     main()
