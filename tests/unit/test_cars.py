aa5708789ec167c86a6b82df603bfccdafba74ee
emma <emma@solebrity.me>
2015-06-23 21:48:25 -0400

    Use tan function to convert degrees to proper premultiplier, must test on other quadrants

--- tests/unit/test_cars.py
+++ tests/unit/test_cars.py
@@ -13,7 +13,46 @@ class TestCarBasicFunctionality(TestCase):
         self.assertIsNotNone(car.speed)
         self.assertIsNotNone(car.direction)
 
-#    def test_car_
+
+class Test_Car_exists_in_space_time_continuum(TestCase):
+    def test_drive_changes_cars_location(self):
+        tesla = cars.Car()
+        initial_x, initial_y = tesla.x_loc, tesla.y_loc
+        tesla.drive(steps=10)
+        self.assertNotEqual((initial_x, initial_y),
+                            (tesla.x_loc, tesla.y_loc))
+
+    def test_drive_changes_location_as_fn_input(self):
+        car1 = cars.Car()
+        car2 = cars.Car()
+        self.assertEqual((car1.x_loc, car1.y_loc),
+                         (car2.x_loc, car2.y_loc))
+        car1.drive(steps=1)
+        car2.drive(steps=10)
+        self.assertNotEqual((car1.x_loc, car1.y_loc),
+                            (car2.x_loc, car2.y_loc))
+
+
+    def test_drive_distance_is_steps_times_size(self):
+        fiat = cars.Car()
+        mercedes = cars.Car()
+        fiat.drive(steps=10, speed=100)
+        mercedes.drive(steps=10, speed=10)
+        self.assertLess(mercedes.y_loc, fiat.y_loc)
+
+    def test_drive_direction_affects_x_and_y_magnitues(self):
+        accord = cars.Car()
+        accord.direction=30
+        accord.drive(speed=109.8)
+        self.assertEqual(accord.x_loc, 95)
+        self.assertEqual(accord.y_loc, 55)
+
+    def test_premultiplier_does_its_math_right(self):
+        car = cars.Car()
+        x, y = car.dist_premultiplier(degrees=30, distance=109.8)
+        self.assertEqual(x, 95)
+        self.assertEqual(y, 55)
+
 
 if __name__ == "__main__":
     main()
