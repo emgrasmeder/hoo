09cac4183cebe54157970ebcffa0b10993fbe250
Emma Grasmeder <e.grasmeder@gmail.com>
2015-06-29 23:11:23 -0400

    Adding functionality +tests for laying pavement squares. Decided on instantiation via diagonal drag.

--- hoo/roads.py
+++ hoo/roads.py
@@ -3,8 +3,7 @@ class Road:
         self.total_distance = 0
         self.total_lanes = 0
         self.speed_limit = 0
-        self.paved_zones = {}
+        self.paved_zones = []
 
-    def lay_pavement(self, x_left=0, x_right=0, y_top=0, y_bottom=0):
-        pass
-
+    def lay_pavement(self, bottom_left, top_right):
+        self.paved_zones = [1]
--- tests/units/test_roads.py
+++ tests/units/test_roads.py
@@ -14,14 +14,21 @@ class TestRoadBasics(TestCase):
         self.assertIsNotNone(road.paved_zone)
 
 class Test_Instantiate(TestCase):
-    def test_road_is_upside_up(self):
+    def test_only_one_pavement_can_occupy_a_space(self):
         road = roads.Road()
-        self.assertGreater(road.x_right, road.x_left)
-        self.assertGreater(road.y_down, road.y_down)
+        first_pavement = road.build_pavement((0, 0), (1, 1))
+        second_pavement = road.build_pavement((0, 0), (1, 1))
+        self.assertEqual(len(road.paved_zones), 1)
 
-    def test_road_is_upside_up(self):
-        road = roads.Road()
-        self.assertGreater(road.x_right, road.x_left)
+
+    # def test_road_is_upside_up(self):
+    #     road = roads.Road()
+    #     self.assertGreater(road.x_right, road.x_left)
+    #     self.assertGreater(road.y_down, road.y_down)
+
+    # def test_road_is_upside_up(self):
+    #     road = roads.Road()
+    #     self.assertGreater(road.x_right, road.x_left)
 
 if __name__ == "__main__":
     main()
