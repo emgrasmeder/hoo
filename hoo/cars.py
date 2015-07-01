8e4d422d34cd3ef8ad7cff8a7ed07accef8eb45b
Emma Grasmeder <e.grasmeder@gmail.com>
2015-07-01 18:03:00 -0400

    Add functional tests, which FAILS when asking if cars turn around at edge of road

--- hoo/cars.py
+++ hoo/cars.py
@@ -1,5 +1,6 @@
 from hoo import roads
 
+
 class Car:
     def __init__(self, road=None):
 
@@ -18,8 +19,6 @@ class Car:
         degrees = degrees % 360
         y_dist = round(hypotenuse * sin(radians(degrees)), 2)
         x_dist = round(hypotenuse * cos(radians(degrees)), 2)
-        print("a2 + b2 = {}".format(round((x_dist**2) + (y_dist**2))))
-        print("d2 = {}".format(distance**2))
         assert round((x_dist**2) + (y_dist**2)) == round(hypotenuse**2)
         if degrees in [0, 360]:
             x_dist = distance
@@ -33,14 +32,13 @@ class Car:
         if degrees == 270:
             x_dist = 0
             y_dist = -1 * distance
-        print("x_dist: {}".format(x_dist))
-        print("y_dist: {}".format(y_dist))
         return x_dist, y_dist
 
-    def drive(self, speed=1):
+    def drive(self, speed=1, direction=None):
         """
         """
-
+        if isinstance(direction, int):
+            self.direction = direction
         x_dist, y_dist = self.dist_premultiplier(self.direction,
                                                  distance=speed)
         self.x_loc += x_dist
--- tests/functional/test_highway_driving.py
+++ tests/functional/test_highway_driving.py
@@ -1,19 +1,42 @@
 from unittest import TestCase, main
-import random
-from hoo import roads
+from hoo import cars
 
 class Test_CarsGoOnRoads(TestCase):
-    def test_cars_instantiate_on_a_road(self):
-        pass
-        ## Probably useless ##
-        # # In the beginning, we make the earth
-        # # (now there exists time)
-        # world = worlds.World()
-        ##
+    def test_cars_drive_around_without_leaving_road(self):
 
+        # When we make a car, it comes with its own default road
+        car = cars.Car()
+        # and that car's road has some x,y coordinates that define it
 
-        # When we first make a car,
-        # placed on a NxN plot of Road()
+        self.assertEqual(car.road.paved_area["bottom_left"],
+                         (-100, -100))
+        self.assertEqual(car.road.paved_area["top_left"],
+                         (-100, 100))
+        self.assertEqual(car.road.paved_area["top_right"],
+                         (100, 100))
+        self.assertEqual(car.road.paved_area["bottom_right"],
+                         (100, -100))
+
+        # The car begins in the middle of the road
+        self.assertEqual((car.x_loc, car.y_loc), (0, 0))
+
+        # but it can drive in any direction it want (degrees as input!)
+
+        car.drive(direction=90)
+        self.assertEqual(
+            (car.x_loc, car.y_loc),
+            (0, 1)
+                         )
+
+        # until it comes to a dead end, in which case it
+        # picks a new direction and carries on
+
+        for i in range(1000):
+            car.drive()
+            self.assertLessEqual(car.y_loc, 100)
+            self.assertGreaterEqual(car.y_loc, -100)
+            self.assertLessEqual(car.x_loc, 100)
+            self.assertGreaterEqual(car.x_loc, -100)
 
 
 
