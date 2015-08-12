825a6391a95c1a168d1476ac01fd7134f65625d2
Emma Grasmeder <e.grasmeder@gmail.com>
2015-08-12 17:17:47 -0400

    weird atom additions

---
+++ tests/units/plots_test.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from hoo import plots
+
+class Test_data_is_findable(TestCase):
+    def test_basic_simulation_creates_findable_logfile(self):
+        pass
+
+
+if __name__ == "__main__":
+    from unittest import main
+    main()
